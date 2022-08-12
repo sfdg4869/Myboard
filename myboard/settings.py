b3ed571b8b1750cf81a93bec9c8225ee045a997e
sfdg4869 <sfdg4869@gmail.com>
2022-08-12 12:03:49 +0900

    Commit message

--- myboard/settings.py
+++ myboard/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['secret_key']
+SECRET_KEY = os.environ.get['DJANGO_SECRET_KEY', '@t*lbby*@(v=8teybe^+m*svsh$8dst@--g)y1_3p1zv*^7o49']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
